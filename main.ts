e93d7bc78dd68cd56b70b44e99642266b78c67b8
anagabyperalta03 <110991307+anagabyperalta03@users.noreply.github.com>
2022-10-25 11:26:44 -0700

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+music.setTempo(120)
+for (let index = 0; index < 2; index++) {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+}
+for (let index = 0; index < 2; index++) {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Double))
+}
